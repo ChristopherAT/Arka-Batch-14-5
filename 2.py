b7496fe040c999a3699a75f68f9116e5bfa53a53
Christopher <christopher.at99@gmail.com>
2020-01-25 11:46:41 +0700

    Edit regex, ada yang beda dari soal batch sebelumnya

--- 2.py
+++ 2.py
@@ -1,21 +1,21 @@
 import re
 def usernameValidity(username):
-    format = r'^[a-z][\w]{4,11}'
+    format = r'^[a-z][a-z_\.]{7,11}'
     # format regex: [a-z]  diawali huruf kecil 
-    #               [\w]   tidak menggunakan special character kecuali unedrscore
-    #               {4,11} repitisi format diatas, min 4 karakter 
-    #                      dan max 14 karakter
+    #               [a-z_\.]  hanya menerima huruf kecil, titik, dan underscore
+    #               {7,11} repitisi format sebelumnya, min 7 karakter 
+    #                      dan max 11 karakter
     return bool(re.fullmatch(format,username))
 def passwordValidity(password):
     # cek syarat satu-persatu agar lebih mudah dibaca
-    if bool(re.fullmatch(r'^[0-9A-Z\W]{7}$',password)):
-      # If bernilai True jika password berupa 7 karakter 
+    if bool(re.fullmatch(r'^[\w\W]{9}$',password)):
+      # If bernilai True jika password berupa 9 karakter 
       # dengan kombinasi angka, huruf besar, dan simbol
-      if len(re.findall(r'[0-9]',password))==1:
-        # If bernilai True jika password memuat 1 angka
-        if len(re.findall(r'[\W]',password))==1:
-          # If bernilai True jika password memuat 1 simbol
+      if len(re.findall(r'[0-9]',password))>=1:
+        # If bernilai True jika password memuat min 1 angka
+        if len(re.findall(r'[\W]',password))>=1:
+          # If bernilai True jika password memuat min 1 simbol
           # return True, karena password adalah kombinasi 
-          # 1 simbol, 1 angka dan 5 huruf besar
+          # 1 simbol, 1 angka dan 7 huruf besar
           return True
     return False
